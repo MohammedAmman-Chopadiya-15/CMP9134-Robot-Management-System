2eb4ba738d3b528589b8b28231d10000d72a729f
MohammedAmman Chopadiya <ammanchopadiya15@gmail.com>
2026-03-04 09:30:49 +0000

    Adding Homepage to test backend-frontend connection

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Allow your React app to talk to the Backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"message": "Cloud Backend Active"}
+    return {"message": "Cloud Backend Active - Hello from FastAPI!"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Loading...")
+
+  //Calling FastAPI
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/')
+      .then(response => {
+        setMessage(response.data.message)
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error)
+        setMessage("Backend is not responding!")
+      })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Robot Management System</h1>
+      <div style={{ padding: '20px', border: '1px solid #ccc', display: 'inline-block' }}>
+        <p>Message from Backend:</p>
+        <h2 style={{ color: '#007bff' }}>{message}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
